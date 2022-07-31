8b2aace54e861d012a0e25e3cca0c24bcec116aa
tmg2549 <tmg2549@utexas.edu>
2022-07-31 14:09:07 -0500

    Set up Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import SubmissionContainer from './components/SubmissionContainer'
 import PostsContainer from './components/PostsContainer'
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained">Hello World</Button>
       <SubmissionContainer/>
       <PostsContainer/>
     </div>
