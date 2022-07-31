82f1d82b06a2aadedbb078593e37cf9d97ef3c5b
tmg2549 <tmg2549@utexas.edu>
2022-07-31 13:54:24 -0500

    Configure Redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import SubmissionContainer from './components/SubmissionContainer'
+import PostsContainer from './components/PostsContainer'
 
 function App() {
   return (
     <div className="App">
-      
+      <SubmissionContainer/>
+      <PostsContainer/>
     </div>
   );
 }
