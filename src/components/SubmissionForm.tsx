bea0eedeb88e8ae07bdbe09d21bec3b3afe6bfe9
tmg2549 <tmg2549@utexas.edu>
2022-07-31 20:55:36 -0500

    Set up handler for submitting form

--- src/components/SubmissionForm.tsx
+++ src/components/SubmissionForm.tsx
@@ -1,32 +1,56 @@
 import React, {useState, SyntheticEvent} from 'react'
 import Button, {} from '@mui/material/Button'
 import SendIcon from '@mui/icons-material/Send';
-import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
-import FormControl from '@mui/material/FormControl';
 
 function SubmissionForm() {
-  const [image, setImage] = useState<string>("https://protkd.com/wp-content/uploads/2017/04/default-image.jpg")
+  // state for image preview
+  const defaultImageURL = "https://protkd.com/wp-content/uploads/2017/04/default-image.jpg";
+  const [image, setImage] = useState<string>(defaultImageURL);
 
-  function onChange(e: SyntheticEvent){
-    const target: any = e.target
+  // changes preview image to uploaded image
+  function onFileUpload(e: SyntheticEvent){
+    const target: any = e.target;
     const file = target.files[0];
-    const reader = new FileReader()
+    const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
         if (typeof reader.result === "string"){
-          setImage(reader.result)
+          setImage(reader.result);
         }
     }
-    
+    // resets default image if upload fails
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+      console.log('Resetting to default image');
+      setImage(defaultImageURL);
+    }
+  }
+  // sends POST request with image file and comment to server
+  async function handleFormSubmit(e: SyntheticEvent){
+    e.preventDefault();
+    const target: any = e.target;
+    const imageFile = target.elements.image.files[0];
+    const comment = target.elements.comment;
+
+    const data = new FormData()
+    data.append('image', imageFile)
+    data.append('comment', comment)
+
+    // const fetchOptions = {
+    //   method: 'POST',
+    //   body: data
+    // }
+
+    // const response = await fetch('/upload', fetchOptions)
   }
 
   return (
-    <form>
-      <Input type="file" onChange={onChange}/>
+    <form onSubmit={handleFormSubmit}>
+      <Input name="image" type="file" onChange={onFileUpload}/>
       <img src={image} alt="Preview for post"></img>
-      <Input placeholder="Enter comment here..."></Input>
-      <Button variant="contained" endIcon={<SendIcon />}>Submit post</Button>
+      <Input name="comment" placeholder="Enter comment here..."></Input>
+      <Button type="submit" variant="contained" endIcon={<SendIcon />}>Submit post</Button>
     </form>
   )
 }
