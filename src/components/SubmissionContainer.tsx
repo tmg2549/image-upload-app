7f7a76a05b07c5d28a740d0db47842aabcece462
tmg2549 <tmg2549@utexas.edu>
2022-08-01 21:14:37 -0500

    Wrap SubmissionContainer in Card

--- src/components/SubmissionContainer.tsx
+++ src/components/SubmissionContainer.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import SubmissionForm from './SubmissionForm'
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card'
 
 function SubmissionContainer() {
   return (
     <section>
-      <Typography variant="h1">Post your posts here!!!</Typography>
-      <Typography variant="subtitle1">Show us your artsy side. Post your pet, brunch, and sunset pics here. </Typography>
-      <SubmissionForm/>
+      <Card variant="outlined" sx={{m: 16, mb: 4, p: 4, maxWidth: 'md', borderRadius: 4}}>
+        <Typography variant="h2">Post your posts here!!!</Typography>
+        <Typography variant="subtitle2" sx={{mb: 2}}>Show us your artsy side. Post your pet, brunch, and sunset pics here. </Typography>
+        <SubmissionForm/>
+      </Card>
     </section>
   )
 }
